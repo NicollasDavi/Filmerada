ae55b14ca341eaddc1159e5ea4d520c90f3d1a43
NicollasDavi <132391248+NicollasDavi@users.noreply.github.com>
2024-01-29 23:24:45 -0300

    movie api request full

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { Link, Outlet } from 'react-router-dom'
+import {Outlet } from 'react-router-dom'
 import Navbar from './components/Navbat'
 
 function App() {
   return (
     <div className='App'>
       <Navbar/>
-      <h2>FILMERADA</h2>
       <Outlet></Outlet>
     </div>
   )
