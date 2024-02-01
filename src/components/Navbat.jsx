7e68b18749bebeb58234b4ed95d05e81966fe089
Nicollas <148489209+SacPositivo@users.noreply.github.com>
2024-02-01 10:35:23 -0300

    nav

--- src/components/Navbat.jsx
+++ src/components/Navbat.jsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom'
 import {BiCameraMovie, BiSearchAlt2} from 'react-icons/bi'
+import styles from './Navbar.module.css'
 
 const Navbar = () => {
   return (
-      <nav id='navbar'>
+      <nav id='navbar' className={styles.navbar}>
         <h2>
           <Link to="/"><BiCameraMovie/>FILMERADA</Link>
         </h2>
@@ -13,7 +14,7 @@ const Navbar = () => {
                 <BiSearchAlt2/>
             </button>
         </form>
-        <Link to="/movie/:id">Filmes</Link>
+        <Link to="/movie/:id" className={styles.link}>Filmes</Link>
         <Link to="/search">Search</Link>
       </nav>
   )
